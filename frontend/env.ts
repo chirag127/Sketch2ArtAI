91d6e03164f3071392acadd03cf8835a5e4b41ed
Chirag <76880977+chirag127@users.noreply.github.com>
2025-03-16 22:20:47 +0530

    Remove local API_URL configurations and retain production API_URL

--- frontend/env.ts
+++
@@ -1,4 +0,0 @@
-// Replace 192.168.1.X with your actual local IP address
-// export const API_URL = "http://192.168.31.232:4000/api";
-// export const API_URL = "http://localhost:4000/api";
-export const API_URL = "https://sketch2artai.onrender.com/api";
